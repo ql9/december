e842d585b019af81da7b6db6bb5511807a7efe23
Quốc Lê <ql9.lms@gmail.com>
2021-01-02 10:00:14 +0700

    final commit

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening as port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening as port ${PORT}`);
 });
